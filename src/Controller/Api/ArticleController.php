3635423642978825a0c7c9be48451ec3ec855d3f
rhenardM <rhenardmunongo@gmail.com>
2024-03-19 23:22:00 +0100

    mise en forme

--- src/Controller/Api/ArticleController.php
+++ src/Controller/Api/ArticleController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Summarizer\Summarizer;
+//use Summarizer\Summarizer;
 
 class ArticleController extends AbstractController
 {
